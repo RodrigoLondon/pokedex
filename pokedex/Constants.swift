f79c5aabfb2656c932aba0aa2024c9328c1d0aa5
Lewis Jones <apple@apples-macbook.home>
2016-03-24 18:56:22 +0000

    pokedex

---
+++ pokedex/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Lewis Jones on 14/03/2016.
+//  Copyright © 2016 Rodrigo Pena. All rights reserved.
+//
+
+import Foundation
+
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
